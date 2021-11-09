524c3a1a258922d13bdd874047133ec82e0f0823
MohammadAmin-Mashayekhan <mashayekhan.mohammadamin@gmail.come>
2021-11-09 09:02:19 +0330

    some changes

--- Draw.php
+++ Draw.php
@@ -1,77 +1,64 @@
 <html>
-	<head>
-		<meta charset="UTF-8">
-		<title>Document</title>
-		<link rel="stylesheet" href="Draw.css">
-	</head>
-	<body>
 
-		<form method="post">
-			Height: 	<input type="number" name="height" placeholder="Enter height number" value="10">
-			Fill 			<input type="radio" name="fill" value=1 checked>
-			Border 	<input type="radio" name="fill" value=0>
-			<input type="submit"  value="Draw">
-		</form>
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" href="Draw.css">
+</head>
 
-		<?php
-			if ($_SERVER["REQUEST_METHOD"]== "POST")
-			 {
-			 	$height =$_POST['height'];
-			 	$fill =$_POST['fill'];
-				// var_dump($fill);
+<body>
 
-		 		 if (empty($height))
-			 	 {
-			 		echo "Height is empty";
-			 		return;
-			 	 }
-				else
-			 	 {
-			 			draw_square($height , $fill);
-			 	 }
+	<form method="post">
+		Height: <input type="number" name="height" placeholder="Enter height number" value="10">
+		Fill <input type="radio" name="fill" value=1 checked>
+		Border <input type="radio" name="fill" value=0>
+		<input type="submit" value="Draw">
+	</form>
 
-			 }
+	<?php
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$height = $_POST['height'];
+		$fill = $_POST['fill'];
+		// var_dump($fill);
 
-			function draw_square($_height = 5 , $_fill = true )
-			{
+		if (empty($height)) {
+			echo "Height is empty";
+			return;
+		} else {
+			draw_square($height, $fill);
+		}
+	}
 
-					echo('<br><pre>');
-					for ($row=0; $row <$_height ; $row++)
-					 {
-						for ($col=0; $col <$_height ; $col++)
-						 {
-						 	if ($_fill)
-						   	 {
-						   	 		echo "*";
-						 	 }
-						 	 else
-						 	 {
-						   	 		if ($col==0 || $col==$_height-1 || $row==0 || $row==$_height-1)
-						   	 		 {
-						 	 			echo "*";
-						 	 		 }
-						 	 		 else
-						 	 		 {
-						 	 		 	echo"  ";
-						 	 		 }
-							 }
+	function draw_square($_height = 5, $_fill = true)
+	{
 
-						 }
-						 echo("<br>");
-					 }
-					 echo('</pre>');
+		echo ('<br><pre>');
+		for ($row = 0; $row < $_height; $row++) {
+			for ($col = 0; $col < $_height; $col++) {
+				if ($_fill) {
+					echo "*";
+				} else {
+					if ($col == 0 || $col == $_height - 1 || $row == 0 || $row == $_height - 1) {
+						echo "*";
+					} else {
+						echo "  ";
+					}
+				}
+			}
+			echo ("<br>");
+		}
+		echo ('</pre>');
+	}
 
-			  }
+	?>
 
-		 ?>
+	<footer>
+		<br>
 
-        	        <footer>
-        		                <br>
-
-                          Guided by:  <a target="_blank" href="www.ermile.com"> : Mr.Javad Evazzadeh</a>
-        		                <br>
-                        		Designed by <a target="_blank" href="www.hayate-ensani.com">Mohammad amin Mashayekhan.</a>
-        	        </footer>
+		Designed by <a target="_blank" href="https://github.com/MohammadAmin-Mashayekhan">Mohammad amin Mashayekhan.</a>
+		<br>
+		Guided by: <span>Mr. Javad Adib</span>
+	</footer>
 
 
 
@@ -81,5 +68,6 @@
 
 
 
-	</body>
+</body>
+
 </html>
